5500e462b80e33c56aad74b153fad8892419e512
首飞 <907575489@qq.com>
2020-09-19 18:04:11 +0800

    完善文档

--- osqp-eigen/example/src/simpleqp_example.cpp
+++ osqp-eigen/example/src/simpleqp_example.cpp
@@ -34,67 +34,67 @@ int main()
     Eigen::VectorXd upperBound; //u
 
 //下面的实例来源于 https://ww2.mathworks.cn/help/optim/ug/quadprog.html?s_tid=srchtitle
-//具有线性约束的二次规划
-    // hessian.resize(2,2);
-    // hessian.insert(0,0) = 1;
-    // hessian.insert(1,0) = -1;
-    // hessian.insert(0,1) = -1;
-    // hessian.insert(1,1) = 2;
-    // std::cout << "hessian:" << std::endl << hessian << std::endl;
+// 具有线性约束的二次规划
+//     hessian.resize(2,2);
+//     hessian.insert(0,0) = 1;
+//     hessian.insert(1,0) = -1;
+//     hessian.insert(0,1) = -1;
+//     hessian.insert(1,1) = 2;
+//     std::cout << "hessian:" << std::endl << hessian << std::endl;
 
-    // gradient.resize(2);
-    // gradient << -2, -6;
+//     gradient.resize(2);
+//     gradient << -2, -6;
     
-    // std::cout << "gradient:" << std::endl << gradient << std::endl;
+//     std::cout << "gradient:" << std::endl << gradient << std::endl;
 
-    // linearMatrix.resize(3,2);
-    // linearMatrix.insert(0,0) = 1;
-    // linearMatrix.insert(0,1) = 1;
-    // linearMatrix.insert(1,0) = -1;
-    // linearMatrix.insert(1,1) = 2;
-    // linearMatrix.insert(2,0) = 2;
-    // linearMatrix.insert(2,1) = 1;
-    // std::cout << "linearMatrix:" << std::endl << linearMatrix << std::endl;
+//     linearMatrix.resize(3,2);
+//     linearMatrix.insert(0,0) = 1;
+//     linearMatrix.insert(0,1) = 1;
+//     linearMatrix.insert(1,0) = -1;
+//     linearMatrix.insert(1,1) = 2;
+//     linearMatrix.insert(2,0) = 2;
+//     linearMatrix.insert(2,1) = 1;
+//     std::cout << "linearMatrix:" << std::endl << linearMatrix << std::endl;
 
-    // lowerBound.resize(3);
-    // lowerBound << -OsqpEigen::INFTY, -OsqpEigen::INFTY, -OsqpEigen::INFTY;
-    // std::cout << "lowerBound:" << std::endl << lowerBound << std::endl;
+//     lowerBound.resize(3);
+//     lowerBound << -OsqpEigen::INFTY, -OsqpEigen::INFTY, -OsqpEigen::INFTY;
+//     std::cout << "lowerBound:" << std::endl << lowerBound << std::endl;
 
-    // upperBound.resize(3);
-    // upperBound << 2, 2, 3;
-    // std::cout << "upperBound:" << std::endl << upperBound << std::endl;
+//     upperBound.resize(3);
+//     upperBound << 2, 2, 3;
+//     std::cout << "upperBound:" << std::endl << upperBound << std::endl;
 
-    // int NumberOfVariables = 2; //A矩阵的列数
-    // int NumberOfConstraints = 3; //A矩阵的行数
+//     int NumberOfVariables = 2; //A矩阵的列数
+//     int NumberOfConstraints = 3; //A矩阵的行数
 
-//具有线性等式约束的二次规划
-    // hessian.resize(2,2);
-    // hessian.insert(0,0) = 1;
-    // hessian.insert(1,0) = -1;
-    // hessian.insert(0,1) = -1;
-    // hessian.insert(1,1) = 2;
-    // std::cout << "hessian:" << std::endl << hessian << std::endl;
+// 具有线性等式约束的二次规划
+//     hessian.resize(2,2);
+//     hessian.insert(0,0) = 1;
+//     hessian.insert(1,0) = -1;
+//     hessian.insert(0,1) = -1;
+//     hessian.insert(1,1) = 2;
+//     std::cout << "hessian:" << std::endl << hessian << std::endl;
 
-    // gradient.resize(2);
-    // gradient << -2, -6;
+//     gradient.resize(2);
+//     gradient << -2, -6;
     
-    // std::cout << "gradient:" << std::endl << gradient << std::endl;
+//     std::cout << "gradient:" << std::endl << gradient << std::endl;
 
-    // linearMatrix.resize(1,2);
-    // linearMatrix.insert(0,0) = 1;
-    // linearMatrix.insert(0,1) = 1;
-    // std::cout << "linearMatrix:" << std::endl << linearMatrix << std::endl;
+//     linearMatrix.resize(1,2);
+//     linearMatrix.insert(0,0) = 1;
+//     linearMatrix.insert(0,1) = 1;
+//     std::cout << "linearMatrix:" << std::endl << linearMatrix << std::endl;
 
-    // lowerBound.resize(1);
-    // lowerBound << 0;
-    // std::cout << "lowerBound:" << std::endl << lowerBound << std::endl;
+//     lowerBound.resize(1);
+//     lowerBound << 0;
+//     std::cout << "lowerBound:" << std::endl << lowerBound << std::endl;
 
-    // upperBound.resize(1);
-    // upperBound << 0;
-    // std::cout << "upperBound:" << std::endl << upperBound << std::endl;
+//     upperBound.resize(1);
+//     upperBound << 0;
+//     std::cout << "upperBound:" << std::endl << upperBound << std::endl;
 
-    // int NumberOfVariables = 2; //A矩阵的列数
-    // int NumberOfConstraints = 1; //A矩阵的行数
+//     int NumberOfVariables = 2; //A矩阵的列数
+//     int NumberOfConstraints = 1; //A矩阵的行数
 
 
 //具有线性约束和边界的二次最小化
@@ -153,13 +153,14 @@ int main()
     solver.settings()->setWarmStart(true);
 
     // set the initial data of the QP solver
-    solver.data()->setNumberOfVariables(NumberOfVariables);
-    solver.data()->setNumberOfConstraints(NumberOfConstraints);
-    if(!solver.data()->setHessianMatrix(hessian)) return 1;
-    if(!solver.data()->setGradient(gradient)) return 1;
-    if(!solver.data()->setLinearConstraintsMatrix(linearMatrix)) return 1;
-    if(!solver.data()->setLowerBound(lowerBound)) return 1;
-    if(!solver.data()->setUpperBound(upperBound)) return 1;
+    //矩阵A为m*n矩阵
+    solver.data()->setNumberOfVariables(NumberOfVariables); //设置A矩阵的列数，即n
+    solver.data()->setNumberOfConstraints(NumberOfConstraints); //设置A矩阵的行数，即m
+    if(!solver.data()->setHessianMatrix(hessian)) return 1;//设置P矩阵
+    if(!solver.data()->setGradient(gradient)) return 1; //设置q or f矩阵。当没有时设置为全0向量
+    if(!solver.data()->setLinearConstraintsMatrix(linearMatrix)) return 1;//设置线性约束的A矩阵
+    if(!solver.data()->setLowerBound(lowerBound)) return 1;//设置下边界
+    if(!solver.data()->setUpperBound(upperBound)) return 1;//设置上边界
 
     // instantiate the solver
     if(!solver.initSolver()) return 1;
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 1. osqp-master是从[https://github.com/oxfordcontrol/osqp](https://github.com/oxfordcontrol/osqp)克隆下来的0.60版本的osqp库。里面包含了qdldl库，所以无需重新下载。
 2. osqp-eigen是对osqp库的eigen接口封装，使其更加好用。
 3. osqp-eigen/example/src/simpleqp_example.cpp是参考[MATLAB帮助文档](https://ww2.mathworks.cn/help/optim/ug/quadprog.html?s_tid=srchtitle)写的简单例子。主要是为了方便理解osqp-eigen的接口如何使用。
-4. 快速开始使用该库可以参考我的[博文]()。
+4. 快速开始使用该库可以参考我的[博文](https://blog.csdn.net/shoufei403/article/details/108672152)。
